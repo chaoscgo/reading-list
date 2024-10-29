d422de76a873beda244cabd899329647a97638a6
Beverly <chaoscgo@hotmail.com>
2024-10-28 19:58:46 -0500

    added auth controller

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const morgan = require("morgan");
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session')
+
+const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -19,6 +22,22 @@ mongoose.connection.on("connected", () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(morgan('dev'));
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+      user: req.session.user,
+    });
+});
+
+app.use('/auth', authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);

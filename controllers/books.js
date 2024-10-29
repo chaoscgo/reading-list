f0ec3be3d57a6a0a7211eabef96e271263cf0e08
Beverly <chaoscgo@hotmail.com>
2024-10-29 09:48:42 -0500

    added create functionality

--- controllers/books.js
+++ controllers/books.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+
+//index route
 router.get('/', (req, res) => {
     try {
       res.render('books/index.ejs');
@@ -12,8 +14,40 @@ router.get('/', (req, res) => {
     }
 });
 
+//new route
 router.get('/new', async (req, res) => {
   res.render('books/new.ejs');
 });
+
+
+//create route
+router.post('/', async (req, res) => {
+  try {
+
+    if (req.body.finishedReading === "on") {
+      req.body.finishedReading = true;
+    } else {
+      req.body.finishedReading = false;
+    }
+
+    if (req.body.recommend === "on") {
+      req.body.recommend = true;
+    } else {
+      req.body.recommend = false;
+    }
+  
+    const currentUser = await User.findById(req.session.user._id);
+
+    currentUser.books.push(req.body);
+    await currentUser.save();
+ 
+    res.redirect(`/users/${currentUser._id}/books`);
+  } catch (error) {
+ 
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
 
 module.exports = router;
